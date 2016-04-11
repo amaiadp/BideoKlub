2c3df81d5847948955c05e71351a70f25586fb3b
Mielotxim <ekaneiza8@gmail.com>
2016-04-11 13:13:21 +0200

    KONEXIOA klasea prest ( STATE : alpha )

---
+++ src/grafiko/Konexioa.java
@@ -0,0 +1,11 @@
+package grafiko;
+
+public class Konexioa {
+	private static Konexioa nKonekzioa = null;
+	
+	private Konexioa(){
+		
+	}
+	
+	// QUEDA TOdO EL CODIGO xD (PARA LA TARDE PONDRE LA BASE DEL CODIGO)
+}
