3c99ff806fd0206fe4c209ce319808bd6b2eee8b
amaia <amitxu96@gmail.com>
2016-04-25 12:40:04 +0200

    konexioa

--- src/grafiko/Konexioa.java
+++ src/grafiko/Konexioa.java
@@ -5,61 +5,68 @@ import java.util.ArrayList;
  
 public class Konexioa {
  
-    public static void main(String[] args) throws Exception{
-        getConnection();
-        post();
-        select();
-    }
-   
-    public static Connection getConnection() throws Exception{
+	private Connection conn;
+	private static Konexioa nKonexioa;
+	
+	
+	
+	private Konexioa(){
+		this.getConnection();
+	}
+
+	public static Konexioa getKonexioa(){
+		if(nKonexioa== null){
+			new Konexioa();
+		}
+		return nKonexioa;
+	}
+	
+	
+	
+    private Connection getConnection(){
         try{
             String driver = "com.mysql.jdbc.Driver";
-            String url = "jdbc:mysql://localhost:3306/database";
-            String username = "hey";
-            String password = "mypass";
+            String url = "jdbc:mysql://localhost:3306/bideoklub";
+            String username = "root";
+            String password = "";
             Class.forName(driver);
            
-            Connection conn = DriverManager.getConnection(url,username,password);
+            conn = DriverManager.getConnection(url,username,password);
             System.out.println("Connected");
             return conn;
         }catch(Exception e){System.out.println(e);}
         return null;
     }
    
-    public static void post() throws Exception{
-        final String var1 = "john";
-        final String var2 = "miller";
+    public void post(String pAgindu){
         try {
-            Connection conn = getConnection();
-            PreparedStatement posted = conn.prepareStatement("INSERT INTO table (first, last) VALUES ('"+var1+"', '"+var2+"')");
+            PreparedStatement posted = conn.prepareStatement(pAgindu);
            
             posted.executeUpdate();
            
         }catch(Exception e){System.out.println(e);}
-        finally{
-            System.out.println("Insert Completed");
-        }
     }
    
-    public static ArrayList<String> select() throws Exception{
+    public ResultSet select(String pAgindua){
         try{
             Connection con = getConnection();
-            PreparedStatement statement = con.prepareStatement("SELECT first, last FROM table");
+            PreparedStatement statement = con.prepareStatement(pAgindua);
            
             ResultSet result = statement.executeQuery();
+            return result;
            
-            ArrayList<String> array = new ArrayList<String>();
-            while(result.next()){
-                System.out.print(result.getString("first"));
-                System.out.print(("    "));
-                System.out.println(result.getString("last"));
-               
-               
-                array.add(result.getString("last"));
-               
-            }
-            System.out.println("All records have been selected");
-            return array;
+//           	ArrayList<String> array = new ArrayList<String>();
+//            while(result.next()){
+//                System.out.print(result.getString("first"));
+//                System.out.print(("    "));
+//                System.out.println(result.getString("last"));
+//               
+//               
+//                array.add(result.getString("last"));
+//               
+//            }
+//            System.out.println("All records have been selected");
+//            return array;
         }catch (Exception e){System.out.println(e);}
         return null;
    
