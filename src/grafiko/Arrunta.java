3af42b97348c85ba9e3b1e1cf223dceab5df01a2
zihara <zihara@zihara-CX61-2QC>
2016-04-29 18:29:10 +0200

    Katalogoa ikus daiteke

--- src/grafiko/Arrunta.java
+++ src/grafiko/Arrunta.java
@@ -1,12 +1,17 @@
 package grafiko;
 
 import java.awt.BorderLayout;
+import java.awt.Font;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.ResultSet;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.border.EmptyBorder;
 
 public class Arrunta extends JFrame{
@@ -29,7 +34,7 @@ public class Arrunta extends JFrame{
 		JButton btnNewButton = new JButton("Katalogoa ikusi");
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				
+				katalogoaErakutsi();
 			}
 		});
 		panel.add(btnNewButton);
@@ -50,6 +55,46 @@ public class Arrunta extends JFrame{
 		});
 		panel.add(btnNewButton4);
 		setVisible(true);
+		
+	}
+	
+	private void katalogoaErakutsi(){
+		JPanel panel = new JPanel();
+		panel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		panel.setLayout(new BorderLayout(30,30));
+		JPanel p = new JPanel();
+		p.setLayout(new GridLayout(80,3)); //79 pelikula baino gutxiago badaude
+		Integer i;
+		String s;
+		try{
+			JLabel l = new JLabel("Kodea	");
+			l.setFont(new Font("Arial", Font.PLAIN, 15));
+			p.add(l);
+			l = new JLabel("Egoera	");
+			l.setFont(new Font("Arial", Font.PLAIN, 15));
+			p.add(l);
+			l = new JLabel("Titulua			");
+			l.setFont(new Font("Arial", Font.PLAIN, 15));
+			p.add(l);
+			ResultSet ema = Konexioa.getKonexioa().select("SELECT kodea,egoera,titulua FROM Pelikula;");
+			while(ema.next()){
+				i = ema.getInt("kodea");
+				s = i.toString();
+				l = new JLabel(s);
+				l.setFont(new Font("Arial", Font.PLAIN, 15));
+				p.add(l);
+				l = new JLabel(ema.getString("egoera"));
+				l.setFont(new Font("Arial", Font.PLAIN, 15));
+				p.add(l);
+				l = new JLabel(ema.getString("titulua"));
+				l.setFont(new Font("Arial", Font.PLAIN, 15));
+				p.add(l);
+			}
+			JScrollPane jsp = new JScrollPane(p);
+			panel.add(jsp);
+			
+		}catch(Exception ex){System.out.println(ex);}
+		
 		
 	}
 	
